0cda60afe0bf121d3968bef1dbd80ef805513dd4
jeongsanghyeon <sh199919@gmail.com>
2022-09-15 00:05:11 +0900

    Add refresh button

--- app/src/main/java/com/example/calder/MainActivity.kt
+++ app/src/main/java/com/example/calder/MainActivity.kt
@@ -5,15 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -23,13 +33,14 @@ class MainActivity : ComponentActivity() {
     companion object {
         private const val frameDelay = 16L // 16ms. for 60 FPS
 
-        private const val complexity = 15
         private const val canvasFillRatio = 0.8f
     }
 
     private var canvasSize = mutableStateOf<IntSize?>(null)
     private var compositionBitmap = mutableStateOf<Bitmap?>(null)
-    private var firstInit = true
+    private var drawJob: Job? = null
+
+    private var complexity = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,30 +48,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             LaunchedEffect(canvasSize.value) {
                 canvasSize.value?.let { size ->
-                    if (firstInit) {
-                        firstInit = false
+                    if (drawJob == null) {
                         init(size)
                     }
                 }
             }
 
-            Canvas(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .onGloballyPositioned {
-                        canvasSize.value = it.size
-                    }
             ) {
-                compositionBitmap.value?.let {
-                    drawImage(
-                        image = it.asImageBitmap(),
-                        topLeft = Offset(
-                            center.x - it.width / 2,
-                            center.y - it.height / 2
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .onGloballyPositioned {
+                            canvasSize.value = it.size
+                        }
+                ) {
+                    compositionBitmap.value?.let {
+                        drawImage(
+                            image = it.asImageBitmap(),
+                            topLeft = Offset(
+                                center.x - it.width / 2,
+                                center.y - it.height / 2
+                            )
                         )
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth().padding(16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_refresh_24),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(androidx.compose.ui.graphics.Color.Gray),
+                        modifier = Modifier
+                            .size(100.dp)
+                            .clip(RoundedCornerShape(20))
+                            .clickable { refresh() }
+                            .border(
+                                1.dp,
+                                androidx.compose.ui.graphics.Color.LightGray,
+                                RoundedCornerShape(20)
+                            )
+                            .padding(16.dp)
                     )
                 }
             }
+
+        }
+    }
+
+    private fun refresh() {
+        complexity = (1..10).random()
+        canvasSize.value?.let { size ->
+            drawJob?.cancel()
+            init(size)
         }
     }
 
@@ -68,11 +114,11 @@ class MainActivity : ComponentActivity() {
         val adjustedSize = size.width.coerceAtMost(size.height) * canvasFillRatio
         val bitmapSize = RectF(0f, 0f, adjustedSize, adjustedSize)
 
-        val compositionRectArray = (0..complexity).map {
+        val compositionRectArray = (0 until complexity).map {
             CompositionRect(bitmapSize)
         }
 
-        lifecycleScope.launch {
+        drawJob = lifecycleScope.launch {
             while (true) {
                 val temp = Bitmap.createBitmap(
                     adjustedSize.toInt(),
