f715e5cec49d4c65997b676808bd24b56b84dff7
jeongsanghyeon <sh199919@gmail.com>
2022-09-14 22:46:12 +0900

    Composition create POC

--- app/src/main/java/com/example/calder/MainActivity.kt
+++ app/src/main/java/com/example/calder/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.calder
 
-import android.graphics.Bitmap
+import android.R.attr.bitmap
+import android.graphics.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntSize
+
 
 class MainActivity : ComponentActivity() {
 
@@ -21,6 +22,15 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val compositionRectArray = (0 .. 4).map { CompositionRect( Rect(0, 0, 1000, 1000)) }
+        val temp = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+        Canvas(temp).run {
+            compositionRectArray.forEach {
+                it.draw(this)
+            }
+        }
+        compositionBitmap.value = temp
 
         setContent {
             Canvas(
@@ -42,5 +52,44 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
 
+class CompositionRect(
+    parentRect: Rect
+){
+    enum class CompositionRectType{
+        TOP, RIGHT, BOTTOM, LEFT
+    }
+
+    val compositionRectType = CompositionRectType.values().random()
+
+    val anchorPoint = Point(
+        (0 until parentRect.width()).random(),
+        (0 until parentRect.height()).random(),
+    )
+
+    var defaultRect = when(compositionRectType){
+        CompositionRectType.TOP -> Rect(0, 0, parentRect.width(), anchorPoint.y)
+        CompositionRectType.RIGHT -> Rect(anchorPoint.x, 0, parentRect.width(), parentRect.height())
+        CompositionRectType.BOTTOM -> Rect(0, anchorPoint.y, parentRect.width(), parentRect.height())
+        CompositionRectType.LEFT -> Rect(0, 0, anchorPoint.x, parentRect.height())
+    }
+
+    var rectColor = Color.valueOf(
+        getRandomColorValue(), getRandomColorValue(), getRandomColorValue()
+    ).toArgb()
+
+    fun draw(canvas: Canvas){
+        canvas.run {
+            drawRect(
+                defaultRect,
+                Paint().apply {
+                    color = rectColor
+                    xfermode = PorterDuffXfermode(PorterDuff.Mode.LIGHTEN)
+                }
+            )
+        }
+    }
+
+    fun getRandomColorValue() = (0 until 255).random().toFloat()
 }
